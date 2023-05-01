e8620a98f4953ef7d61155ec9da9e52cd8f93587
Syaeful Bahri <syaefulbahri@protonmail.com>
2023-05-01 20:15:37 +0700

    Nambain router admin

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ var logger = require("morgan");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
+// Router Admin
+const adminRouter = require("./routes/admin");
+
 var app = express();
 
 // view engine setup
@@ -28,6 +31,8 @@ app.use(
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+// admin
+app.use("/admin", adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
